423a66a471125180f4560d5b54daaaa0606e5386
heesang0224 <heesang0224@gmail.com>
2025-10-09 05:03:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ console.warn("started");
 
 import { world, system } from "@minecraft/server";
 
-// 플레이어가 블록을 부술 때마다 채팅에 메시지 출력
-world.afterEvents.playerBreakBlock.subscribe((event) => {
-	const player = event.player;
-	player.sendMessage("블록을 부쉈습니다!");
+world.afterEvents.entityHitEntity.subscribe((event) => {
+    const entity = event.entity;
+    
+    if (entity.id === "minecraft:player") {
+        entity.sendMessage("플레이어가 엔티티를 공격했습니다!");
+    }
 });
-
