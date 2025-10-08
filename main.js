f923aa2f04389f5d888d8c3c66ec5a662494b7b3
heesang0224 <heesang0224@gmail.com>
2025-10-09 04:17:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 console.warn("started");
 
-import { world, WorldBeforeEvents } from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 
-world.afterEvents.blockExplode.subscribe(function(data){
-    world.sendMessage("a player broke a block");
-    
-})
+// 플레이어가 블록을 부술 때마다 채팅에 메시지 출력
+world.afterEvents.playerBreakBlock.subscribe((event) => {
+	const player = event.player;
+	player.sendMessage("블록을 부쉈습니다!");
+});
 
