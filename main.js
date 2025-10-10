a5238ff47ce3fa3a1278c29a99587fc1086ac0ec
heesang0224 <heesang0224@gmail.com>
2025-10-10 19:30:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ console.warn("script started");
 
 import { world } from "@minecraft/server";
 
-world.afterEvents.playerPlaceBlock.subscribe((event) => {
-    const player = event.player;
-    const block = event.block;
-    player.runCommand(`say You placed a ${block.typeId}`);
-});
+world.afterEvents.itemUse.subscribe((event)=>{
+    const item = event.itemStack;
+    const player = event.source;
+
+    if(item.typeId == "minecraft:stick"){
+        player.applyKnockback(2, player.location.x, player.location.z);
+        console.warn(`${player.name} 넉백됨`);
+    }
+})
