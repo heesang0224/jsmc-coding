3553ea3aee33d65624a3e18b9babb4b27607a642
heesang0224 <heesang0224@gmail.com>
2025-10-10 14:15:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,8 @@ console.warn("script started");
 
 import { world } from "@minecraft/server";
 
-world.afterEvents.itemStartUse.subscribe((event) => {
-    const item = event.itemStack;
-    const player = event.source;
-    if (item.typeId == "minecraft:iron_axe") {
-        player.runCommand('summon tnt ~ ~ ~ {"Fuse":0}');
-    }
-
-    if (item.typeId == "minecraft:stick"){
-        player.spawnParticle("minecraft:flame", player.location, 20);
-    }
+world.afterEvents.playerPlaceBlock.subscribe((event) => {
+    const player = event.player;
+    const block = event.block;
+    player.runCommand(`say You placed a ${block.typeId}`);
 });
