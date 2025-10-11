e3e47091d07b6fa3595f1e98c34ed3e1cbebab53
heesang0224 <heesang0224@gmail.com>
2025-10-11 12:58:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ console.warn("script started");
 
 import { world } from "@minecraft/server";
 
-world.afterEvents.playerBreakBlock.subscribe((ev) => {
-    const block = ev.block;
-    const player = ev.player;
 
-    // 다이아몬드 광석을 부쉈는지 확인
-    if (block.typeId === "minecraft:diamond_ore") {
-        // 플레이어에게 막대기 1개 지급
-        player.runCommandAsync('give @s stick 1');
+world.afterEvents.blockExplode.subscribe((ev)=>{
+    const block = ev.block;
+    const player = ev.source;
+    if(block.typeId == "minecraft:diamond_block"){
+        player.runCommand("give @s diamond 10");
     }
 });
