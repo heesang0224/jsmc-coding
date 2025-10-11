ccca59a2b6bb144554c34b6feedb9d6967a96dba
heesang0224 <heesang0224@gmail.com>
2025-10-11 12:38:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 console.warn("script started");
 
-import { world } from "@minecraft/server";
+import { world, system } from "@minecraft/server";
 
-world.afterEvents.itemUse.subscribe((event)=>{
-    const item = event.itemStack;
-    const player = event.source;
+world.afterEvents.blockExplode.subscribe((ev)=>{
+    const block = ev.block;
+    const player = ev.source;
 
-    if(item.typeId == "minecraft:stick"){
-        player.applyKnockback(2, player.location.x, player.location.z);
-        console.warn(`${player.name} 넉백됨`);
-    }
-})
+    if(block.typeId == "minecraft:diamond_block"){
+        player.runCommand(`give @s diamond 10`);
+    }});
