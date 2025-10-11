ab2c111c27629f98c236700957c9d901b6a9c8a8
heesang0224 <heesang0224@gmail.com>
2025-10-11 20:07:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ console.warn("script started");
 import { world } from "@minecraft/server";
 
 
-world.afterEvents.blockExplode.subscribe((ev)=>{
-    const block = ev.block;
-    const player = ev.source;
-    if(block.typeId == "minecraft:diamond_block"){
-        player.runCommand("give @s diamond 10");
+world.afterEvents.chatSend.subscribe((ev)=>{
+    const msg = ev.message;
+
+    if(msg == "kill count"){
+        ev.sender.runCommand("say 카운트 시작")
+        ev.sender.runCommand("scoreboard objectives add kill dummy kill")
+        ev.sender.runCommand("scoreboard objectives setdisplay sidebar kill")
+        ev.sender.runCommand("scoreboard players add @a kill 0")
     }
+    if(msg == "reset"){
+        ev.sender.runCommand("say 카운트 초기화")
+        ev.sender.runCommand("scoreboard players set @a kill 0")
+    }
+})
+
+
+world.afterEvents.entityDie.subscribe((ev)=>{
+    const kill = ev.damageSource.damagingEntity
+    const die =ev.deadEntity
+    if(kill.typeId == "minecraft:player"){
+        if(die.typeId == "minecraft:player"){
+
+            world.sendMessage(`${kill.name}님이 ${die.name}님을 처치하였습니다!`)
+            kill.runCommand(`scoreboard players add @s kill 1`)
+        }
+    }
+
 });
