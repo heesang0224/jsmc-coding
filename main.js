adc24fd83f1b349337e3a30aded391c86a7c771f
heesang0224 <heesang0224@gmail.com>
2025-11-01 10:51:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-console.warn("stared");
+console.warn("starting");
 
-import { world } from "@minecraft/server";
+import {world} from "@minecraft/server";
 
-world.afterEvents.chatSend.subscribe((event)=>{
-    const message = event.message;
-    const player = event.sender;
-
-    if(message == "uhc start"){
-        player.runCommand("give @a stone_sword 1");
-        player.runCommand("enchant @a sharpness 2");
-        player.runCommand("give @a stone_pickaxe 1");
-        player.runCommand("enchant @a efficiency 5");
-        player.runCommand("give @a stone_axe 1");
-        player.runCommand("enchant @a efficiency 5");
-        player.runCommand("give @a stone_shovel 1");
-        player.runCommand("enchant @a efficiency 5");
-        
-    }
+world.afterEvents.playerEmote.subscribe((event)=>{
+    const player = event.player;
+    const emote = event.personaPieceId;
+    console.warn(`${player.name} just did the emote: ${emote}`);
 })
