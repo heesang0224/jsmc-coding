4ceb3a9ec282f7a2c733c0785b07a2df40f662ea
heesang0224 <heesang0224@gmail.com>
2025-11-01 16:01:41 +0900

    Add event listeners for block actions and entity damage

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-console.warn("starting");
-
 import {world} from "@minecraft/server";
 
-world.afterEvents.playerEmote.subscribe((event)=>{
-    const player = event.player;
-    const emote = event.personaPieceId;
-    console.warn(`${player.name} just did the emote: ${emote}`);
+world.afterEvents.playerBreakBlock.subscribe((event)=>{
+    const block = event.brokenBlockPermutation;
+    const player= event.player;
+    player.runCommand(`summon lightning_bolt ${player.location.x} ${player.location.y} ${player.location.z}`)});
+
+
+world.afterEvents.entityHurt.subscribe((event)=>{
+    const entity = event.hurtEntity;
+    const damager = event.damageSource.damagingEntity;
+    if(damager?.typeId === "minecraft:player"){
+        damager.applyDamage(5);
+    } 
+})
+
+world.afterEvents.playerPlaceBlock.subscribe((event)=>{
+    const block = event.block;
+    const player= event.player;
+    player.onScreenDisplay.setActionBar(`You placed a ${block.typeId}`);
 })
